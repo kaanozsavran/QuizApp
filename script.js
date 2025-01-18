d7afcce31d59cda8586274e8eff9f220dc23a6af
kaanozsavran <kaanozsavran@hotmail.com>
2025-01-18 15:45:38 +0300

    update for checking answer options

--- script.js
+++ script.js
@@ -29,16 +29,24 @@ const quiz = new Quiz(sorular);
 document.querySelector(".btn_start").addEventListener('click', function () {
     document.querySelector(".quiz_box").classList.add("active");
     soruGoster(quiz.soruGetir());
+    document.querySelector(".next_btn").classList.remove("show");
 })
 
 document.querySelector(".next_btn").addEventListener("click", function () {
     if (quiz.sorular.length != quiz.soruIndex + 1) {
         quiz.soruIndex += 1;
         soruGoster(quiz.soruGetir());
+        document.querySelector(".next_btn").classList.remove("show");
     } else {
         console.log('quiz bitti.')
     }
 });
+
+const option_list = document.querySelector(".option_list");
+const correctIcon = '<div class="icon"><i class="fas fa-check"></i></div>';
+const incorrectIcon = '<div class="icon"><i class="fas fa-times"></i></div>';
+
+
 
 function soruGoster(soru) {
     let question = `<span>${soru.soruMetni}</span>`;
@@ -53,6 +61,30 @@ function soruGoster(soru) {
             `;
     }
     document.querySelector(".question_text").innerHTML = question;
-    document.querySelector(".option_list").innerHTML = options;
+    option_list.innerHTML = options;
+
+    const option = option_list.querySelectorAll(".option");
+
+    for (let opt of option) {
+        opt.setAttribute("onclick", "optionSelected(this)")
+    }
+}
+
+function optionSelected(option) {
+    let cevap = option.querySelector("span b").textContent;
+    let soru = quiz.soruGetir();
+
+    if (soru.cevabiKontrolEt(cevap)) {
+        option.classList.add("correct");
+        option.insertAdjacentHTML("beforeend", correctIcon);
+    } else {
+        option.classList.add("incorrect");
+        option.insertAdjacentHTML("beforeend", incorrectIcon);
+    }
+
+    for (let i = 0; i < option_list.children.length; i++) {
+        option_list.children[i].classList.add("disabled");
+    }
+    document.querySelector(".next_btn").classList.add("show");
 
 }
